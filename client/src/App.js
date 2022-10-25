a70115663a85723e715bdcf7a9e6735819302246
abdelhaq-97 <nouhiabdelhak5@gmail.com>
2022-10-25 20:20:41 +0100

    added home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
-import HomePage from "./components/HomePage";
+// import HomePage from "./components/HomePage";
+import Login from "./components/Login"
 import Footer from "./components/Footer";
 
 
@@ -7,7 +8,8 @@ function App() {
   return (
     <div className="App">
       <Header title= 'Travels' />
-      <HomePage />
+      <Login />
+      {/* <HomePage /> */}
       <Footer />
     </div>
   )
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,9 @@
 
 const Login = () => {
     return (
-        <div>
-            <div className="bg-[#372948] flex justify-center flex-col gap-8 mx-3.5 my-12  p-12 rounded">
-                <h1 className="font-bold text-white text-2xl">Login</h1>
+        <div className="flex justify-center mt-24">
+            <div className="form w-2/6 flex flex-col gap-6 bg-gradient-to-r from-sky-500 to-indigo-500 p-8 text-white font-medium rounded-lg">
+                <h1 className="font-bold text-xl">login</h1>
                 <div className="flex flex-col gap-3 text-white">
                     <label>E-mail</label>
                     <input type="email" placeholder="E-mail" className=" px-2 py-2 rounded text-black" />
@@ -13,8 +13,15 @@ const Login = () => {
                     <input type="password" placeholder="Password" className=" px-2 py-2 rounded text-black" />
                 </div>
 
-                <button className=" px-2 py-2 rounded text-black bg-[#ffcaca] font-bold text-center">Login</button>
+                <button className="bg-gradient-to-r from-slate-900 to-slate-600 hover:from-pink-500 hover:to-yellow-500 p-2 rounded-md">Login</button>
+            </div>
 
+            <div className="">
+                <img
+                    className="max-w-sm rounded-2xl mt-28 "
+                    src={require("../assets/images/chinh-le-duc-8DG9zdUcxVg-unsplash(1).jpg")}
+                    alt="we care family"
+                />
             </div>
         </div>
     )
