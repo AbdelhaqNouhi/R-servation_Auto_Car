ee9276dd132499dbcb53bea986b1ed3f2576d78b
abdelhaq-97 <nouhiabdelhak5@gmail.com>
2022-11-10 18:10:50 +0100

    Added

--- client/src/components/Admin/NavBar.js
+++ client/src/components/Admin/NavBar.js
@@ -7,11 +7,8 @@ const NavBar = () => {
 
             <div class="">
                 <input className='items-center w-40 md:w-80 max-w-xs px-3 py-1 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-600  focus:ring-sky-600' type='text' placeholder='Shearch' />
-                
-                {/* <div class="flex absolute inset-y-0 items-center pl-40">
-                    <img className='w-4 h-4' src={require("../../assets/icons/search.png")}></img>    
-                </div> */}
             </div>
+            
             <div className='flex space-x-4 items-center'>
                 <h1 className='hover:text-sky-600'>Abdelhaq Nouhi</h1>
                 <img src={require("../../assets/images/user-image.JPG")} className='object-cover inset-0 h-9 w-9 rounded-full'></img>
