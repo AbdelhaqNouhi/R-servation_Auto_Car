0d7a1e92028045a65ba875229df4cfb76aa60c4c
abdelhaq-97 <nouhiabdelhak5@gmail.com>
2022-11-10 16:51:46 +0100

    added NavBar Admin component

--- client/src/components/Admin/Admin.js
+++ client/src/components/Admin/Admin.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from './NavBar'
+import NavItems from './NavItems'
 
 const Admin = () => {
     return (
         <div>
             <NavBar />
+            <NavItems />
         </div>
     )
 }
--- client/src/components/Admin/NavBar.js
+++ client/src/components/Admin/NavBar.js
@@ -2,15 +2,23 @@ import React from 'react'
 
 const NavBar = () => {
     return (
+        <header className='lg:container md:px-16 sm:px-10 px-4 bg-white shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] py-5 flex justify-between items-center'>
+            <h1 className=' text-black hover:text-sky-600 font-bold text-2xl'>Travel</h1>
 
-        <div className='container bg-white shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] py-5 flex justify-between'>
-            <h1 className='text-black hover:text-sky-600 font-bold text-2xl'>Admin</h1>
-            <div>
-                <img src='' alt=''></img>
-                <p className='text-black hover:text-sky-600 font-bold text-2xl'>Abdelhaq Nouhi</p>
+            <div class="">
+                <input className='items-center w-40 md:w-80 max-w-xs px-3 py-1 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-600  focus:ring-sky-600' type='text' placeholder='Shearch' />
+                
+                {/* <div class="flex absolute inset-y-0 items-center pl-40">
+                    <img className='w-4 h-4' src={require("../../assets/icons/search.png")}></img>    
+                </div> */}
             </div>
-        </div>
+            <div className='flex space-x-4 items-center'>
+                <h1>Abdelhaq Nouhi</h1>
+                <img src={require("../../assets/images/user-image.JPG")} className='object-cover inset-0 h-9 w-9 rounded-full'></img>
+            </div>
+        </header>
     )
+    added NavBar Admin component
 }
 
 export default NavBar
